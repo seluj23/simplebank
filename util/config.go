dbfa4eef6140b62b0d83e0df5589105e102dd5ea
Ivan Jules Vivas <mr.ijules@gmail.com>
2024-07-05 18:53:51 +0800

    Add login endpoint

--- util/config.go
+++ util/config.go
@@ -1,15 +1,19 @@
 package util
 
 import (
+	"time"
+
 	"github.com/spf13/viper"
 )
 
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variables.
 type Config struct {
-	DBDriver      string `mapstructure:"DB_DRIVER"`
-	DBSource      string `mapstructure:"DB_SOURCE"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	DBDriver            string        `mapstructure:"DB_DRIVER"`
+	DBSource            string        `mapstructure:"DB_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSecretKey      string        `mapstructure:"TOKEN_SECRET_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION" `
 }
 
 func LoadConfig(path string) (config Config, err error) {
